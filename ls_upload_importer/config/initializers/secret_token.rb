9793a8d88d488d0e9ead1798d16aaeee82d0774a
Todd Pickell <tapickell@gmail.com>
2014-07-09 16:42:10 -0500

    initial app creation

---
+++ ls_upload_importer/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LsUploadImporter::Application.config.secret_key_base = 'f360a13212d340f86a9a5a24db8e9a11fb8847ab81b36302e1e6cfc8859be5096d23b44e21fb13db37dc36c27ed02dc20f88a2649ead907edc835cb1b412f8f9'
---
+++ ls_upload_importer/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LsUploadImporter::Application.config.session_store :cookie_store, key: '_ls_upload_importer_session'
